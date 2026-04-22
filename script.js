7c61df1247383692d54a6c0703810364bc13d739
Burak Angun <burak.angun22@gmail.com>
2026-04-22 15:53:58 +0300

    circle creation on clicked coordinate

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-            
+console.log("Ready to go")
+alert("Script is workingg(??).")
+const plank= document.getElementById("plank");
+
+let objects = [];
+
+plank.addEventListener("click",(e)=> {
+
+    const bounds = plank.getBoundingClientRect();
+    const x = e.clientX- bounds.left;
+    const distance = x-200;
+    const weight = Math.floor(Math.random() * 10) + 1;
+
+    objects.push({weight,distance});
+
+    const circle = document.createElement("div");
+    circle.className = "weight-of-object";
+
+    circle.style.left = `${x}px`;
+
+    circle.innerText = weight;
+    plank.appendChild(circle);
+    console.log("Daire eklendi: ", {weight, distance});
+});
