26ff381404f839ed393084fc944af1e3da02d571
Burak Angun <buraak380@hotmail.com>
2026-04-22 17:31:59 +0300

    torque calculation and display

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 console.log("Ready to go")
 alert("Script is workingg(??).")
 
-const container = document.querySelector(".simulation-container");
+const container= document.querySelector(".simulation-container");
 const plank= document.getElementById("plank");
 
 let objects = [];
 
 container.addEventListener("click",(e)=> {
+    
+        const bounds = container.getBoundingClientRect();
+        const x = e.clientX- bounds.left;
 
-    const bounds = container.getBoundingClientRect();
-    const x = e.clientX- bounds.left;
+        if(x >= 0 && x <= 400){
+            const distance=x-200;
+            const weight = Math.floor(Math.random() * 10) + 1;
+            
+            newObject = {weight, distance};
+            objects.push(newObject);
 
-    const plankStartX = 0;
-    const plankEndX = 400;
+            const circle = document.createElement("div");
+            circle.className = "weight-of-object";
 
-    if(x >= plankStartX && x <= plankEndX){
-        const distance = x-200;
-        const weight = Math.floor(Math.random() * 10) + 1;
+            circle.style.left = `${x}px`;
 
-        objects.push({weight,distance});
+            circle.innerText = weight;
+            plank.appendChild(circle);
+            console.log("Daire eklendi: ", {weight, distance});
 
-        const circle = document.createElement("div");
-        circle.className = "weight-of-object";
-
-        circle.style.left = `${x}px`;
-
-        circle.innerText = weight;
-        plank.appendChild(circle);
-        console.log("Daire eklendi: ", {weight, distance});
+            calculateTorque();
     }
+    });
+function calculateTorque() {
+    let torqueLeft = 0;
+    let torqueRight = 0;
 
-   
-});
+    for(let i = 0; i<objects.length; i++){
+        if(objects[i].distance < 0){
+            torqueLeft += Math.abs(objects[i].distance) * objects[i].weight;
+        } else {
+            torqueRight += objects[i].distance * objects[i].weight;
+        }
+}
+    document.getElementById("left-val").innerText = Math.floor(torqueLeft);
+    document.getElementById("right-val").innerText = Math.floor(torqueRight);
+}
+    
